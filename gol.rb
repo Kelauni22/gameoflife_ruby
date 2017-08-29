5cbdc88b4c3044aab7b523f0ee69ccf41d76da9d
Kelauni22 <Kelauni.Cook@gmail.com>
2017-08-29 19:24:49 -0400

    Rules for game of life

--- gol.rb
+++ gol.rb
@@ -1,1 +1,10 @@
-hey
+#Any live cell w/ fewer than two live neighbors dies
+#Any live cell w/ more than three live neighbors dies
+#Any live cell w/ 2 or 3 live neighbors lives
+#Any dead cell w/ exactly 3 live neighbors becomes live
+
+#start w/ 2 dimensional grid
+#infinite square grid
+#cells can be alive or dead
+#each cell has 8 neighbors
+#each rule applies to each cell simultaneously
