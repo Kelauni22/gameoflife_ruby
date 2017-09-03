cae0018359903565d93d7c87acb456a30e42027a
Kelauni22 <Kelauni.Cook@gmail.com>
2017-09-03 11:25:08 -0400

    Test:PASSED - see if a surrounding cell could change based on the condition of the original cell

--- gol.rb
+++ gol.rb
@@ -13,7 +13,8 @@
 board = Array.new(10){Array.new(10){0}}
 
 #Start a few off as 1
-board[2][2], board[2][3], board[2][4], board[3][3] = 1,1,1,1
+#board[2][2], board[2][3], board[2][4], board[3][3] = 1,1,1,1
+board[2][3] = 1
 
 
 
@@ -24,7 +25,7 @@ board[2][2], board[2][3], board[2][4], board[3][3] = 1,1,1,1
 
 #while loop to keep it going forever until the program quits
 i = 1
-while i < 10 #make this a while true later so that it runs infinitely
+while i < 3 #make this a while true later so that it runs infinitely
   #Draw initial board
   for x in board
     print x
@@ -34,18 +35,21 @@ while i < 10 #make this a while true later so that it runs infinitely
   board.each_with_index do |x, x_index| #get row index number and value
     x.each_with_index do |y, y_index| #get column index number and value
     #check number of alive/dead cells around this particular cell
-    if y == 1 then board[x_index][y_index] = 5 end
+        #board[x_index-1][y_index-1],
+        #board[x_index-1][y_index],
+        #board[x_index-1][y_index+1],
+        #board[x_index][y_index-1],
+        #board[x_index][y_index+1],
+        #board[x_index+1][y_index-1],
+        #board[x_index+1][y_index],
+        #board[x_index+1][y_index+1]
+
+      if y == 1
+        board[x_index][y_index] = "x"
+        board[x_index-1][y_index-1] = 1
+      end
+
 =begin
-      cells_array = [
-        board[x_index-1][y_index-1],
-        board[x_index-1][y_index],
-        board[x_index-1][y_index+1],
-        board[x_index][y_index-1],
-        board[x_index][y_index+1],
-        board[x_index+1][y_index-1],
-        board[x_index+1][y_index],
-        board[x_index+1][y_index+1]
-      ]
       n = cells_array.count(1)
       #check to see if this particular cell is dead or alive
       if y == 1
@@ -63,4 +67,5 @@ while i < 10 #make this a while true later so that it runs infinitely
     end
   end
   i += 1
+  puts ''
 end
